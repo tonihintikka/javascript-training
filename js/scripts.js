bcd10c203168cd1b55c44c6ff69f90b5e4a247b9
Toni Hintikka <toni.hintikka@gmail.com>
2021-01-09 18:57:55 +0200

    adding stuff

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var myValue = "Hello gello";
+var myNum = 20;
+myNum = myNum + 77;
+console.log(myNum);
+
+//let result = prompt("Mikä sun nimes on?", "None");
+
+//console.log(result);
+console.log(myValue);
+console.log(document);
+console.dir(document);
+console.log(document.lastModified);
